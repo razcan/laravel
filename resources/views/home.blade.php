73738a88a1b9059e689464d3fb88ff208a1d120c
razvan mustata <razvan.mustata@gmail.com>
2020-08-11 10:22:40 +0300

    add react comp

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,13 @@
 <html>
 <head>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-<script>
+
+<!-- <script>
 
 var colors = ['red', 'blue', 'green', 'grey', 
                  'black', 'white', 'teal', 'yellow']; 
     var i = 0; 
-    
+
 // $(document).keyup(function(event){
 //     // Get value on button click and show alert
 //     $("#myBtn").click(function(){
@@ -26,22 +27,35 @@ var colors = ['red', 'blue', 'green', 'grey',
   i = i % 9; 
 
 });
-</script>
+</script> -->
 
 <!-- $("#input2,#input1").keyup(function () {
 
 $('#output').val($('#input1').val() * $('#input2').val());
 
 }); -->
-
+<script>
+$('#qty, #price').on('input',function() {
+    var qty = parseInt($('#qty').val());
+    var price = parseFloat($('#price').val());
+    $('#total').val((qty * price ? qty * price : 0).toFixed(2));
+});
+</script>
 
 </head>
 <body>
 
-<input type="text" id="myInput1">
+<tr>
+   <td><input type='text' name='qty' id='qty' value='' />
+   <td><input type='text' name='price' id='price' value='' />
+   <td><input type='text' name='total' id='total' value='' />
+</tr>
+
+
+<!-- <input type="text" id="myInput1">
 <input type="text" id="myInput2">
 <input type="text" id="myInput3">
-<button type="button" id="myBtn">Show Value</button>
+<button type="button" id="myBtn">Show Value</button> -->
 
 </body>
 </html>

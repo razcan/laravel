e962d56f6139f6f418de202b735fbbdbae691984
razvan mustata <razvan.mustata@gmail.com>
2020-08-17 14:24:34 +0300

    rezolvat salvare

--- app/Http/Livewire/Valoare.php
+++ app/Http/Livewire/Valoare.php
@@ -3,20 +3,88 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Contract;
+use App\ContractDetail;
+use Illuminate\Database\Migrations\Migration;
 
 class Valoare extends Component
 {
+
+
+    public $new_contract;
+    public $newContractDetail;
  
 
     public $cantitate;
     public $pret;
-    public $valoare2;
 
-    public function addTodo($cantitate, $pret , $valoare2)
+    public $qtty;
+    public $price;
+    public $value;
+    public $inputContractNumber;
+    public $inputPartner;
+    public $inputCategory;
+    public $inputStartDate;
+    public $inputEndDate;
+    public $item_name;
+    public $idd;
+
+//     public function addTodo($cantitate, $pret , $valoare2)
+// {
+//     return $valoare2;
+
+// }
+
+public function save_contract($inputContractNumber,$inputPartner,$inputStartDate,$inputEndDate,
+$inputCategory,$item_name,$qtty,$price,$value)
 {
-    return $valoare2;
+    
+ $this->value = $this->qtty * $this->price;
+
+ //$this->idd = DB::select('select max(id)id from contracts');
+
+ $this->idd = DB::select("SELECT AUTO_INCREMENT FROM information_schema.TABLES WHERE TABLE_SCHEMA = 'test' AND TABLE_NAME = 'contracts'");
+
+ //$this->idd = $this->idd +1;
+//dd($this->idd[0]->AUTO_INCREMENT);
+
+// dd($this->inputContractNumber,
+// $this->inputPartner,$this->inputStartDate,$this->inputEndDate,
+// $this->inputCategory,$this->item_name,$this->qtty,$this->price,$this->value);
+
+    $contract = Contract::all();
+    $new_contract = new Contract;
+    $new_contract->Partner = $this->inputPartner;
+    $new_contract->ContractNumber =  $this->inputContractNumber;
+    $new_contract->ContractStartDate =  $this->inputStartDate;
+    $new_contract->ContractEndDate =  $this->inputEndDate;
+    $new_contract->ContractCategory =  $this->inputCategory;
+    $new_contract->id =  $this->idd[0]->AUTO_INCREMENT;
+    $new_contract->save();
+
+    
+   $newContractDetail = new ContractDetail;
+   $newContractDetail->item_name =  $this->item_name;
+   $newContractDetail->qtty =  $this->qtty;
+   $newContractDetail->price =  $this->price;
+   $newContractDetail->value =  $this->value;
+   $newContractDetail->contract_id = $this->idd[0]->AUTO_INCREMENT;
+   $newContractDetail->save();
 
 }
+
+
+public function mount()
+    {
+
+        // $this->fill(['valoare2' => 0]);
+
+        //$this->valoare2 = request()->query('valoare2', $this->valoare2);
+
+    }
+
 
 
     public function render()
--- resources/views/livewire/valoare.blade.php
+++ resources/views/livewire/valoare.blade.php
@@ -1,16 +1,88 @@
-<div>
-    <!-- <input wire:model="message" type="text"> -->
 
 
-    <input wire:model.debounce.5ms="cantitate" type="text">
-    <input wire:model.debounce.5ms="pret" type="text">
-    <input type="text" value="{{ $cantitate * $pret }}" >
-
-    <h1>{{ $cantitate * $pret }}</h1>
-
-    <button wire:click="addTodo({{ $cantitate }}, {{ $pret }} , {{ $valoare2 }})">Trimite Request</button>
-    <button wire:click="$set('valoare2', {{ $cantitate * $pret }})">Say Hi</button>
-
-<p> {{ $valoare2 }} </p>
-
+<form wire:submit.prevent="save_contract">
+<div  class="form-group col-xs-4 col-md-4">
+      <label for="inputContractNumber" class="control-label">Contract Number</label>
+      <input 
+      wire:model.debounce.5ms="inputContractNumber"
+      class="form-control"
+      type="text" name="inputContractNumber" id="inputContractNumber">
 </div>
+
+<div  class="form-group col-xs-4 col-md-4">
+      <label for="inputPartner" class="control-label">Partener</label>
+      <input 
+      wire:model.debounce.5ms="inputPartner"
+      class="form-control"
+      type="text" name="inputPartner" id="inputPartner">
+</div>
+
+<div class="form-group col-xs-4 col-md-4">
+    <label for="inputCategory">Categorie Contract</label>
+    <select 
+    wire:model.debounce.5ms="inputCategory"
+    class="form-control" name="inputCategory"  id="inputCategory">
+      <option>Servicii</option>
+      <option>Consultanta</option>
+      <option>Curatenie</option>
+    </select>
+</div>
+
+<div  class="form-group  col-xs-4 col-md-4">
+         <label >Start Data</label>
+         <input 
+         wire:model.debounce.5ms="inputStartDate"
+         type="date" name="StartDate" min="1000-01-01" name="inputStartDate" id="inputStartDate"
+            max="3000-12-31" class="form-control">
+    </div>
+
+    <div  class="form-group col-xs-4 col-md-4">
+        <label >Final Data</label>
+        <input 
+        wire:model.debounce.5ms="inputEndDate"
+        type="date" name="EndDate" min="1000-01-01" name="inputEndDate" id="inputEndDate"
+        max="3000-12-31" class="form-control">
+    </div>
+
+    <div class="form-group col-xs-4 col-md-4">
+    <label for="item_name">Articol</label>
+    <input 
+    wire:model.debounce.5ms="item_name"
+    type="text" class="form-control" id="item_name" name="item_name" aria-describedby="item_name" placeholder="Articol">
+    </div>
+
+    <div class="form-group col-xs-4 col-md-4">
+    <label for="qtty">Cantitate</label>
+    <input 
+    wire:model.debounce.5ms="qtty"
+    type="text" class="form-control" 
+    id="qtty" name="qtty" aria-describedby="qtty" placeholder="Cantitate">
+  </div>
+
+
+  <div class="form-group col-xs-4 col-md-4">
+    <label for="price">Pret</label>
+    <input 
+    wire:model.debounce.5ms="price"
+    type="text" class="form-control" 
+    id="price" name="price" aria-describedby="price" placeholder="Pret">
+  </div>
+
+  <div class="form-group col-xs-4 col-md-4">
+    <label for="Valoare">Valoare</label>
+    <input 
+    value="{{ $qtty * $price }}"
+    type="text" class="form-control" 
+    id="value" name="value" aria-describedby="value" placeholder="value">
+  </div>
+
+
+
+    <!-- <button wire:click="save_contract({{ $qtty }}, {{ $price }}, {{ $qtty * $price }} )">Salvare</button> -->
+    <button type="Submit">Salvare</button>
+
+    <!-- <button wire:click="$set('valoare2', {{ $cantitate * $pret }})">Set</button> -->
+
+
+</form>
+@livewireScripts
